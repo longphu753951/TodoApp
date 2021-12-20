33d5b2939ebb30069afe432f26fbb067d365b98f
longphu753951 <51697950+longphu753951@users.noreply.github.com>
2021-12-20 08:17:51 +0700

    add kasw

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import { Fab, Icon } from 'native-base';
+import { SwipeListView } from 'react-native-swipe-list-view';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 export default class TodoScreen extends Component {
     render() {
         return (
-            <View style={{flex:1}}>
+            <View style={{flex:1, background: 'white'}}>
+                <View>
+                    <Text style={styles.title}>
+                        Today’s tasks
+                    </Text>
+                </View>
                 <Fab
-        position="absolute"
-        size="sm"
-        icon={<FontAwesomeIcon size={30} color='white' icon={ faPlus } />}
-      />
+                    position="absolute"
+                    size="sm"
+                    icon={<FontAwesomeIcon size={30} color='white' icon={ faPlus } />}
+                />
             </View>
         )
     }
 }
+
+const styles = new StyleSheet.create({
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    }
+})
